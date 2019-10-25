77f876777c6356a366bd0c4be25d5eb52bc5535a
Mario Benov <mario.ben@mail.bg>
2019-10-25 10:27:33 +0300

    more feature tests

--- cucumber-examples/features/app.feature
+++ cucumber-examples/features/app.feature
@@ -4,3 +4,19 @@ Feature: Testing the app
   Scenario: The app opens
     When I view the app in the browser
     Then I see the Login page
+
+  Scenario: The user can login
+    When I view the app in the browser
+    #And I enter "testUser" in the first input
+    #And I enter "testPassword" in the second input
+    #And I click on the "Login" button
+    And I login with username "testUser" and password "testPassword"
+    Then I see the main app content
+    #And I wait
+
+  Scenario: There is an error message for invalid username
+    When I view the app in the browser
+    And I login with username "badUser" and password "testPassword"
+    Then I don't see the main app content
+    #And I see an error message
+    #And I wait
--- cucumber-examples/features/step_definitions/app.js
+++ cucumber-examples/features/step_definitions/app.js
@@ -1,4 +1,4 @@
-const { After, Before, Then, When } = require('cucumber')
+const { After, Before, Then, When, defineParameterType, ParameterType } = require('cucumber')
 const { Builder, By } = require('selenium-webdriver')
 const assert = require('assert')
 require('chromedriver')
@@ -23,4 +23,69 @@ Then('I see the Login page', async function() {
   // setTimeout(done, 1000)
   let formElement = await driver.findElement(By.className('login-form'))
   assert(formElement != null)
+})
+
+When('I enter {string} in the first input', async function(username) {
+  let inputElement = await driver.findElement(By.css('.login-form .field:first-child input'))
+  await inputElement.sendKeys(username)
+})
+
+When('I enter {string} in the second input', async function(password) {
+  let inputElement = await driver.findElement(By.css('.login-form .field:nth-child(2) input'))
+  await inputElement.sendKeys(password)
+})
+
+When('I click on the {string} button', async function(btnText) {
+  // let btnElement = await driver.findElement(By.js(function(btnText) {
+  //   return Array.prototype.find.apply(document.getElementsByTagName('button'), (elem) => elem.innerText == btnText)
+  // }, btnText))
+  let buttons = await driver.findElements(By.tagName('button'))
+  let found = buttons.find(async function(btn) {
+    let inner = await btn.getText()
+    return inner == btnText
+  })
+  await found.click()
+})
+
+async function findButton(btnText) {
+  let buttons = await driver.findElements(By.tagName('button'))
+  let found = buttons.find(async function(btn) {
+    let inner = await btn.getText()
+    return inner == btnText
+  })
+  return found
+}
+
+When('I login with username {string} and password {string}', async function(username, password) {
+  let nameInput = await driver.findElement(By.css('.login-form .field:first-child input'))
+  await nameInput.sendKeys(username)
+
+  let passInput = await driver.findElement(By.css('.login-form .field:nth-child(2) input'))
+  await passInput.sendKeys(password)
+
+  let loginBtn = await findButton('Login')
+  await loginBtn.click()
+})
+
+defineParameterType({
+    name: 'seeOrDont',           // name
+    regexp: /(don't |)see/, // regexp
+    //type: Boolean,             // type
+    transformer: s => !s  // transformer function
+})
+// Then(/I (don't |)see the main app content/, async function(shouldBeVisible) {
+Then('I {seeOrDont} the main app content', async function(shouldBeVisible) {
+  let element = await driver.findElement(By.className('main-content'))
+  console.log(shouldBeVisible)
+  if(shouldBeVisible) {
+    assert(element != null)
+  } else {
+    assert(element == null)
+  }
+})
+
+
+When('I wait', () => {
+  //setTimeout(done, 5000)
+  return new Promise((res) => setTimeout(res, 5000))
 })
