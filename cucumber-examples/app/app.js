068c5b2ff6ec06e0300b3ea060c6b52e73282d73
Mario Benov <mario.ben@mail.bg>
2019-10-27 19:26:43 +0200

    add login login

--- cucumber-examples/app/app.js
+++ cucumber-examples/app/app.js
@@ -3,6 +3,15 @@
 const e = React.createElement;
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '', isLoading: false, errorMessage: '' };
+
+    this.onUsernameChange = this.onUsernameChange.bind(this)
+    this.onPasswordChange = this.onPasswordChange.bind(this)
+    this.onLogin = this.onLogin.bind(this)
+  }
+
   render() {
     return React.createElement("div", {
         id: "login"
@@ -10,11 +19,43 @@ class Login extends React.Component {
         className: "login-form"
       }, React.createElement("div", {
         className: "field"
-      }, React.createElement("span", null, "Username"), React.createElement("input", null)), React.createElement("div", {
+      }, React.createElement("span", null, "Username"), React.createElement("input", {
+        value: this.state.username,
+        onChange: this.onUsernameChange
+      })), React.createElement("div", {
         className: "field"
-      }, React.createElement("span", null, "Password"), React.createElement("input", null)), React.createElement("button", {
-        onClick: () => this.props.onLogin()
-      }, "Login")));
+      }, React.createElement("span", null, "Password"), React.createElement("input", {
+        value: this.state.password,
+        onChange: this.onPasswordChange
+      })), this.state.errorMessage.length ? React.createElement("div", {
+        className: "error-message"
+      }, this.state.errorMessage) : null, React.createElement("button", {
+        onClick: this.onLogin,
+        disabled: this.state.isLoading
+      }, "Login")));;
+  }
+
+  onLogin() {
+    this.setState({isLoading: true, errorMessage: ''})
+    setTimeout(() => {
+      this.setState({isLoading: false})
+      if(this.state.username == 'noUser') {
+        return this.setState({errorMessage: 'User does not exist'})
+      }
+      if(this.state.username != 'testUser' && this.state.password != 'testPassword') {
+        return this.setState({errorMessage: 'Incorrect username/password'})
+      }
+
+      this.props.onLogin()
+    }, 1500)
+  }
+
+  onUsernameChange(e) {
+    this.setState({username: e.target.value})
+  }
+
+  onPasswordChange(e) {
+    this.setState({password: e.target.value})
   }
 }
 
@@ -97,26 +138,6 @@ class App extends React.Component {
   }
 }
 
-
-
-// class LikeButton extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { liked: false };
-//   }
-//
-//   render() {
-//     if (this.state.liked) {
-//       return 'You liked this.';
-//     }
-//
-//     return e(
-//       'button',
-//       { onClick: () => this.setState({ liked: true }) },
-//       'Like'
-//     );
-//   }
-// }
 
 const domContainer = document.getElementById('appRoot');
 ReactDOM.render(e(App), domContainer);
